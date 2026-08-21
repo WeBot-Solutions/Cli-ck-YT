f578f9774d5d7a49db1b50db824a1fa7380820c1
Trev7or23 <elguason.5020@gmail.com>
2026-08-21 05:01:52 -0400

    deleting hello world print

--- bin/cli_ck_y_t.dart
+++ bin/cli_ck_y_t.dart
@@ -1,5 +1,3 @@
 import 'package:cli_ck_y_t/cli_ck_y_t.dart' as cli_ck_y_t;
 
-void main(List<String> arguments) {
-  print('Hello world: ${cli_ck_y_t.calculate()}!');
-}
+void main(List<String> arguments) {}

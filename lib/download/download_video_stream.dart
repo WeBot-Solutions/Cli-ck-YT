db65de914fc286ce3c8d2fcf7490c4ba72550f87
yM4rx <guevaramarcos543@gmail.com>
2026-08-21 17:49:24 -0400

    adding things

--- lib/download/download_video_stream.dart
+++ lib/download/download_video_stream.dart
@@ -1,27 +1,48 @@
+import 'dart:collection';
+
 import 'package:cli_ck_y_t/core/exit_with_error.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 class YoutubeDownloader {
   final YoutubeExplode yt;
   final String? id;
-  late final StreamManifest manifest;
+  StreamManifest? manifest;
 
   YoutubeDownloader({required String url})
     : yt = YoutubeExplode(),
       id = VideoId(url).value;
+
+  void downloadMuxedVideo() async {
+
+    manifest ??= await yt.videos.streamsClient.getManifest(id!);
+    var streamInfo = manifest! .muxed.withHigestVideoQuality();
+    
+    // Get the actual byte stream
+var stream = yt.video.streams.get(streamInfo);
+
+// Open a file for writing.
+var file = File(filePath);
+var fileStream = file.openWrite();
+
+// Pipe all the content of the stream into the file.
+await stream.pipe(fileStream);
+
+// Close the file.
+await fileStream.flush();
+await fileStream.close();
+
+
+  } 
 
   void printInfo() {
     print(yt);
     print(id);
   }
 
-  void init() async {
-    manifest = await yt.videos.streamsClient.getManifest(id);
-  }
-
-  void getAudioQualities() {
+  Future<void> getAudioQualities() async {
+    manifest ??= await yt.videos.streamsClient.getManifest(id!);
     print("Audio Qualities Available: ");
-    for (var stream in manifest.audioOnly) {
+    for (var stream in manifest!  .audioOnly) {
       print('- ${stream.bitrate} kbps (${stream.container})');
     }
   }

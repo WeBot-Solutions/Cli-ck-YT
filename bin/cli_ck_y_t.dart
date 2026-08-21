952ebaa2f86166d8a7743b1beecf976094c7f4cf
Trev7or23 <elguason.5020@gmail.com>
2026-08-21 03:54:07 -0400

    first commit

---
+++ bin/cli_ck_y_t.dart
@@ -0,0 +1,5 @@
+import 'package:cli_ck_y_t/cli_ck_y_t.dart' as cli_ck_y_t;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cli_ck_y_t.calculate()}!');
+}
---
+++ test/cli_ck_y_t_test.dart
@@ -0,0 +1,8 @@
+import 'package:cli_ck_y_t/cli_ck_y_t.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

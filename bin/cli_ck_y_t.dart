06320344bc10fce86570e4ec634ac153ba81bf27
Trev7or23 <elguason.5020@gmail.com>
2026-08-22 06:52:35 -0400

    Using The YoutubeDownloader With the getManifest Externally

--- bin/cli_ck_y_t.dart
+++ bin/cli_ck_y_t.dart
@@ -1,22 +1,22 @@
-import 'package:cli_ck_y_t/core/constants.dart';
 import 'package:cli_ck_y_t/core/exit_with_error.dart';
 import 'package:cli_ck_y_t/args/parse_arguments.dart';
 import 'package:cli_ck_y_t/download/download_video_stream.dart';
 
 import 'dart:io';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   if (arguments.isEmpty) exitWithError('Empty Args', exit(1));
 
   var results = parseArguments(arguments);
 
   // Option Values
   var url = results['url'] as String?;
-  var quality = results['quality'] as String? ?? defaultQuality;
   var outputName = results['output'] as String?;
 
   if (url != null) {
-    var ytDownloader = YoutubeDownloader(url: url, output: outputName);
-    ytDownloader.downloadMuxedVideo();
+    var yt = YoutubeDownloader();
+    await yt.getManifest(url);
+
+    yt.downloadMuxedVideo();
   }
 }

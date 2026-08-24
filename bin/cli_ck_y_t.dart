efd95fbc82520c14e24b35447d6fa2f009e44c55
Trev7or23 <elguason.5020@gmail.com>
2026-08-24 03:44:21 -0400

    Using and showing the help print

--- bin/cli_ck_y_t.dart
+++ bin/cli_ck_y_t.dart
@@ -1,13 +1,15 @@
+import 'package:cli_ck_y_t/args/show_help.dart';
 import 'package:cli_ck_y_t/core/exit_with_error.dart';
-import 'package:cli_ck_y_t/args/parse_arguments.dart';
+import 'package:cli_ck_y_t/args/get_parser.dart';
 import 'package:cli_ck_y_t/download/download_video_stream.dart';
 
 import 'dart:io';
 
 void main(List<String> arguments) async {
-  if (arguments.isEmpty) exitWithError('Empty Args', exit(1));
+  var parser = getParser();
+  var results = parser.parse(arguments);
 
-  var results = parseArguments(arguments);
+  if (arguments.isEmpty || results['help']) showHelp(parser);
 
   // Option Values
   var url = results['url'] as String?;

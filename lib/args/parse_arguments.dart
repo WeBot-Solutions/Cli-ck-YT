884f89c6c6ea08e3e64c0c330a5e87efa01e0a5b
Trev7or23 <elguason.5020@gmail.com>
2026-08-24 01:56:03 -0400

    Added flag audio-only

--- lib/args/parse_arguments.dart
+++ lib/args/parse_arguments.dart
@@ -3,6 +3,11 @@ import 'package:args/args.dart';
 ArgResults parseArguments(List<String> args) {
   var parser = ArgParser()
     ..addOption('url', abbr: 'u', help: 'Youtube Video Url')
+    ..addFlag(
+      'audio-only',
+      help: 'Download only Audio`s video',
+      defaultsTo: false,
+    )
     ..addOption('quality', abbr: 'q', help: 'Set Video Quality, default: 480p')
     ..addOption('output', abbr: 'o', help: 'Video Output Name');
 

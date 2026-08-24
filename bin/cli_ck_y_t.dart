b3a56017b6761caf689bb649b3db99f74ae259fb
Trev7or23 <elguason.5020@gmail.com>
2026-08-24 06:22:22 -0400

    updating main

--- bin/cli_ck_y_t.dart
+++ bin/cli_ck_y_t.dart
@@ -1,31 +1,34 @@
+import 'dart:io';
+
 import 'package:cli_ck_y_t/args/show_help.dart';
-import 'package:cli_ck_y_t/core/exit_with_error.dart';
 import 'package:cli_ck_y_t/args/get_parser.dart';
 import 'package:cli_ck_y_t/download/download_video_stream.dart';
-
-import 'dart:io';
 
 import 'package:cli_ck_y_t/utils/extract_youtube_video_id.dart';
 
 void main(List<String> arguments) async {
+  print('loading...');
   var parser = getParser();
   var results = parser.parse(arguments);
 
-  if (arguments.isEmpty || results['help']) showHelp(parser);
-
   // Option Values
   var url = results['url'] as String?;
-  var outputName = results['output'] as String?;
   var audioSelected = results['audio-only'] as bool;
 
-  if (url != null) {
-    var yt = YoutubeDownloader();
-    var videoId = extractYouTubeVideoId(url);
-
-    if (videoId == null) showHelp(parser);
-
-    await yt.getManifest(videoId!);
-
-    audioSelected ? yt.downloadAudio() : yt.downloadMuxedVideo();
+  if (arguments.isEmpty || results['help'] || url == null) {
+    showHelp(parser);
+    return;
   }
+
+  var yt = YoutubeDownloader();
+  var videoId = extractYouTubeVideoId(url);
+
+  if (videoId == null) {
+    showHelp(parser);
+    return;
+  }
+
+  await yt.getManifest(videoId);
+
+  audioSelected ? await yt.downloadAudio() : await yt.downloadMuxedVideo();
 }
